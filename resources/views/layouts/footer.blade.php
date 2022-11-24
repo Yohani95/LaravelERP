9a7c47aba471619ed91b4879068f0b91b5b339d8
Yohani Espinoza <yohani95301@gmail.com>
2022-11-24 11:28:53 -0300

    correccion de pie de pagina

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,6 @@
 <!-- ======= Footer ======= -->
 <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
-<footer id="footer" class="footer">
+<footer id="footer" class="footer fixed-bottom" >
     <div class="copyright">
       &copy; Copyright <strong><span>Digital Solutions</span></strong>. Todos los derechos Reservados
     </div>
